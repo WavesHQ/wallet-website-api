a6b845be15e06be50c190ff036d1ca14e2236eba
JJ Adonis <work@josejai.me>
2023-01-26 14:29:08 +0800

    chore(flags): enable CFP creation

--- src/pages/api/v0/settings/flags.ts
+++ src/pages/api/v0/settings/flags.ts
@@ -305,10 +305,11 @@ export default async function handle(
     {
       id: "ocg_cfp_dfip",
       name: "CFP/DFIP proposal(s)",
-      stage: "beta",
+      stage: "public",
       version: ">2.14.0",
       description: "Allows the submission of CFP/DFIP proposals",
       networks: [
+        EnvironmentNetwork.MainNet,
         EnvironmentNetwork.TestNet,
         EnvironmentNetwork.RemotePlayground,
         EnvironmentNetwork.LocalPlayground,
