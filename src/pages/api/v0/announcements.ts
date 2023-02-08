09abd0f8b711176fffd75dcc769f07ae8fe8d803
Keng Ye <kengyeleow@gmail.com>
2023-02-08 09:02:58 +0800

    chore: prettier formatting

--- src/pages/api/v0/announcements.ts
+++ src/pages/api/v0/announcements.ts
@@ -17,10 +17,8 @@ export default async function handle(
       lang: {
         en: "Find out how to earn returns through negative interest today.",
         de: "Erfahre, wie du noch heute mit Negativzinsen Rendite erzielen kannst.",
-        "zh-Hans":
-          "立即了解如何通过负利率赚取回报。",
-        "zh-Hant":
-          "立即了解如何通過負利率賺取回報。",
+        "zh-Hans": "立即了解如何通过负利率赚取回报。",
+        "zh-Hant": "立即了解如何通過負利率賺取回報。",
         fr: "Découvrez dès aujourd'hui comment obtenir des rendements grâce aux intérêts négatifs.",
         es: "Find out how to earn returns through negative interest today.",
         it: "Find out how to earn returns through negative interest today.",
@@ -43,10 +41,8 @@ export default async function handle(
       lang: {
         en: "Find out how to earn returns through negative interest today.",
         de: "Erfahre, wie du noch heute mit Negativzinsen Rendite erzielen kannst.",
-        "zh-Hans":
-          "立即了解如何通过负利率赚取回报。",
-        "zh-Hant":
-          "立即了解如何通過負利率賺取回報。",
+        "zh-Hans": "立即了解如何通过负利率赚取回报。",
+        "zh-Hant": "立即了解如何通過負利率賺取回報。",
         fr: "Découvrez dès aujourd'hui comment obtenir des rendements grâce aux intérêts négatifs.",
         es: "Find out how to earn returns through negative interest today.",
         it: "Find out how to earn returns through negative interest today.",
