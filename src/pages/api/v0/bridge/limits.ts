a17257266aa1cc3de9eb68a04845d55a6f5f7513
JJ Adonis <work@josejai.me>
2023-02-08 16:04:52 +0800

    feature(limits): expose bridge limits

--- src/pages/api/v0/bridge/limits.ts
+++ src/pages/api/v0/bridge/limits.ts
@@ -2,14 +2,19 @@ import { NextApiRequest, NextApiResponse } from "next";
 import { EnvironmentNetwork } from "@waveshq/walletkit-core";
 // @ts-ignore
 import Cors from "cors";
-import {runMiddleware} from "../../../../utils/middleware";
+import { runMiddleware } from "../../../../utils/middleware";
 
 export const cors = Cors({
   methods: ["GET", "HEAD"],
 });
 
 type BridgeLimits = {
-  [network in EnvironmentNetwork]: { symbol: string; id: string; daily: string; max: string; }[];
+  [network in EnvironmentNetwork]: {
+    symbol: string;
+    id: string;
+    daily: string;
+    max: string;
+  }[];
 };
 
 export default async function handle(
@@ -18,10 +23,15 @@ export default async function handle(
 ): Promise<void> {
   await runMiddleware(req, res, cors);
 
-  const PLAYGROUND_LIMITS = [{ symbol: "BTC", id: "1", daily: "10", max: "100"}]
+  const PLAYGROUND_LIMITS = [
+    { symbol: "BTC", id: "1", daily: "10", max: "100" },
+  ];
 
   res.json({
-    DevNet: [], MainNet: [], Playground: PLAYGROUND_LIMITS, TestNet: [],
-    [EnvironmentNetwork.LocalPlayground]: PLAYGROUND_LIMITS
+    DevNet: [],
+    MainNet: [],
+    Playground: PLAYGROUND_LIMITS,
+    TestNet: [],
+    [EnvironmentNetwork.LocalPlayground]: PLAYGROUND_LIMITS,
   });
 }
