cd569a7f0983c0de9e9b2112f30c043dc3432ad0
veralygit <57183851+veralygit@users.noreply.github.com>
2023-03-14 18:22:38 +0800

    feature(Dex): add get dex stability fees

---
+++ src/pages/api/wallet/dex-stab-fees.ts
@@ -0,0 +1,40 @@
+import BigNumber from "bignumber.js";
+import { NextApiRequest, NextApiResponse } from "next";
+import Cors from "cors";
+import { runMiddleware } from "../../../utils/middleware";
+
+type Fees = {
+  fee: string | BigNumber;
+};
+
+export const cors = Cors({
+  methods: ["POST"],
+});
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<Fees>
+) {
+  await runMiddleware(req, res, cors);
+  const { tokenADisplaySymbol, tokenBDisplaySymbol, pairs } = JSON.parse(
+    req.body
+  );
+
+  let fee;
+
+  const pairWithFees = pairs.filter((pair) => {
+    return (
+      pair.data.displaySymbol.includes("DUSD") && tokenADisplaySymbol === "DUSD"
+    );
+  });
+
+  if (pairWithFees.length === 0) {
+    fee = "0";
+  } else {
+    fee = pairWithFees[0].data.tokenB.fee
+      ? pairWithFees[0].data.tokenB.fee?.pct
+      : pairWithFees[0].data.tokenA.fee?.pct;
+  }
+
+  res.send(fee === "0" ? fee : new BigNumber(fee).multipliedBy(100).toFixed(2));
+}
