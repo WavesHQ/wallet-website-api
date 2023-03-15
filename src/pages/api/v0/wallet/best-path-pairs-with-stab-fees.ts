19f92311ce32bd53f8799758875a422c590951eb
veralygit <57183851+veralygit@users.noreply.github.com>
2023-03-15 19:37:21 +0800

    added try catch to handle errors and update variable namings

--- src/pages/api/v0/wallet/best-path-pairs-with-stab-fees.ts
+++ src/pages/api/v0/wallet/best-path-pairs-with-stab-fees.ts
@@ -30,42 +30,50 @@ export default async function handle(
   await runMiddleware(req, res, cors);
   const { network } = req.query;
 
-  const oceanOptions = newOceanOptions(network);
-  const whaleApiClient = newWhaleAPIClient(oceanOptions);
-  const poolpairs = await whaleApiClient.poolpairs.list(200);
+  try {
+    const oceanOptions = newOceanOptions(network);
+    const whaleApiClient = newWhaleAPIClient(oceanOptions);
+    const poolpairs = await whaleApiClient.poolpairs.list(200);
 
-  const pairsWithFees = poolpairs.filter(
-    (pair) =>
-      (pair.tokenA.displaySymbol === "DUSD" ||
-        pair.tokenB.displaySymbol === "DUSD") &&
-      (pair.tokenA.fee !== undefined || pair.tokenB.fee !== undefined) &&
-      (new BigNumber(pair.tokenA.fee?.pct ?? 0).isGreaterThan(0.01) ||
-        new BigNumber(pair.tokenB.fee?.pct ?? 0).isGreaterThan(0.01))
-  );
+    // Logic for filter would be to
+    // check if either tokenA or tokenB is `DUSD`
+    // check if there is any fee for tokenA or tokenB
+    // check for if the fee is > 0.01 as DEX stabilization fees are more than 0.001 (0.1%)
+    const pairsWithFees = poolpairs.filter(
+      (pair) =>
+        (pair.tokenA.displaySymbol === "DUSD" ||
+          pair.tokenB.displaySymbol === "DUSD") &&
+        (pair.tokenA.fee !== undefined || pair.tokenB.fee !== undefined) &&
+        (new BigNumber(pair.tokenA.fee?.pct ?? 0).isGreaterThan(0.001) ||
+          new BigNumber(pair.tokenB.fee?.pct ?? 0).isGreaterThan(0.001))
+    );
 
-  interface IBestPath {
-    tokenADisplaySymbol: string;
-    tokenBDisplaySymbol: string;
-  }
-
-  const bestPathPairs: IBestPath[] = [];
-
-  // this is to set DUSD as token A for the bestPath mapping
-  for (let i = 0; i < pairsWithFees.length; i += 1) {
-    const pair = pairsWithFees[i];
-    const bestPathPair = {
-      tokenADisplaySymbol: "",
-      tokenBDisplaySymbol: "",
-    };
-    if (pair.tokenA.displaySymbol === "DUSD") {
-      bestPathPair.tokenADisplaySymbol = pair.tokenA.displaySymbol;
-      bestPathPair.tokenBDisplaySymbol = pair.tokenB.displaySymbol;
-    } else {
-      bestPathPair.tokenADisplaySymbol = pair.tokenB.displaySymbol;
-      bestPathPair.tokenBDisplaySymbol = pair.tokenA.displaySymbol;
+    interface IBestPath {
+      tokenADisplaySymbol: string;
+      tokenBDisplaySymbol: string;
     }
-    bestPathPairs.push(bestPathPair);
-  }
 
-  res.json(bestPathPairs);
+    const bestPathPairs: IBestPath[] = [];
+
+    // this is to set DUSD as token A for the bestPath mapping
+    pairsWithFees.forEach((pair) => {
+      const bestPathPair = {
+        tokenADisplaySymbol: "",
+        tokenBDisplaySymbol: "",
+      };
+      if (pair.tokenA.displaySymbol === "DUSD") {
+        bestPathPair.tokenADisplaySymbol = pair.tokenA.displaySymbol;
+        bestPathPair.tokenBDisplaySymbol = pair.tokenB.displaySymbol;
+      } else {
+        bestPathPair.tokenADisplaySymbol = pair.tokenB.displaySymbol;
+        bestPathPair.tokenBDisplaySymbol = pair.tokenA.displaySymbol;
+      }
+      bestPathPairs.push(bestPathPair);
+    });
+
+    res.json(bestPathPairs);
+  } catch (error) {
+    console.log(error);
+    res.json([]);
+  }
 }
--- src/pages/api/v0/wallet/dex-stab-fees.ts
+++ src/pages/api/v0/wallet/dex-stab-fees.ts
@@ -33,30 +33,38 @@ export default async function handler(
   await runMiddleware(req, res, cors);
   const { tokenADisplaySymbol, tokenBDisplaySymbol, network } = req.query;
 
-  const oceanOptions = newOceanOptions(network);
-  const whaleApiClient = newWhaleAPIClient(oceanOptions);
-  const poolpairs = await whaleApiClient.poolpairs.list(200);
-
-  const pairWithDUSD = poolpairs.filter(
-    (pair) =>
-      pair.tokenA.displaySymbol === tokenADisplaySymbol &&
-      pair.tokenB.displaySymbol === tokenBDisplaySymbol &&
-      (pair.tokenA.displaySymbol === "DUSD" ||
-        pair.tokenB.displaySymbol === "DUSD")
-  );
-
   let fee;
-  if (
-    pairWithDUSD.length === 0 ||
-    (pairWithDUSD[0].tokenA.fee === undefined &&
-      pairWithDUSD[0].tokenB.fee === undefined)
-  ) {
-    fee = "0";
-  } else {
-    fee = pairWithDUSD[0].tokenB.fee
-      ? pairWithDUSD[0].tokenB.fee?.pct
-      : pairWithDUSD[0].tokenA.fee?.pct;
-  }
+  try {
+    const oceanOptions = newOceanOptions(network);
+    const whaleApiClient = newWhaleAPIClient(oceanOptions);
+    const poolpairs = await whaleApiClient.poolpairs.list(200);
 
-  res.send(fee === "0" ? fee : new BigNumber(fee).multipliedBy(100).toFixed(2));
+    const filteredTokenPairWithDUSD = poolpairs.filter(
+      (pair) =>
+        pair.tokenA.displaySymbol === tokenADisplaySymbol &&
+        pair.tokenB.displaySymbol === tokenBDisplaySymbol &&
+        (pair.tokenA.displaySymbol === "DUSD" ||
+          pair.tokenB.displaySymbol === "DUSD")
+    );
+
+    if (
+      filteredTokenPairWithDUSD.length === 0 ||
+      (filteredTokenPairWithDUSD[0].tokenA.fee === undefined &&
+        filteredTokenPairWithDUSD[0].tokenB.fee === undefined)
+    ) {
+      fee = "0";
+    } else {
+      fee = filteredTokenPairWithDUSD[0].tokenB.fee
+        ? filteredTokenPairWithDUSD[0].tokenB.fee?.pct
+        : filteredTokenPairWithDUSD[0].tokenA.fee?.pct;
+    }
+
+    res.send(
+      fee === "0" ? fee : new BigNumber(fee).multipliedBy(100).toFixed(2)
+    );
+  } catch (error) {
+    console.log(error);
+    fee = "0";
+    res.send(fee);
+  }
 }
