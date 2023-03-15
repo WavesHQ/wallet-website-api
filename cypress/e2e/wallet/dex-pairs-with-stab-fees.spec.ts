b92a9117f2f3786c66002a84bd157bb4c6d48582
veralygit <57183851+veralygit@users.noreply.github.com>
2023-03-15 11:49:05 +0800

    add api to get pairs with stabilisation fees

---
+++ cypress/e2e/wallet/dex-pairs-with-stab-fees.spec.ts
@@ -0,0 +1,13 @@
+describe("Wallet - Dex Stabilisation Fee", () => {
+  it("should be able to return actual data", () => {
+    cy.request("POST", "/api/wallet/dex-pairs-with-stab-fees").then((resp) => {
+      expect(resp.status).to.eq(200);
+      expect(resp.body).to.deep.equal([
+        { tokenADisplaySymbol: "DUSD", tokenBDisplaySymbol: "DFI" },
+        { tokenADisplaySymbol: "DUSD", tokenBDisplaySymbol: "dUSDT" },
+        { tokenADisplaySymbol: "DUSD", tokenBDisplaySymbol: "dUSDC" },
+        { tokenADisplaySymbol: "DUSD", tokenBDisplaySymbol: "dEUROC" },
+      ]);
+    });
+  });
+});
