7580174fdf4a529926a698ad6bb386d53fc7976d
veralygit <57183851+veralygit@users.noreply.github.com>
2023-03-15 18:50:39 +0800

    fix e2e after changing to req.query

--- cypress/e2e/wallet/dex-stab-fees.spec.ts
+++ cypress/e2e/wallet/dex-stab-fees.spec.ts
@@ -1,12 +1,16 @@
 describe("Wallet - Dex Stabilisation Fee", () => {
   it("should be able to return actual data", () => {
-    cy.request("GET", "/api/v0/wallet/dex-stab-fees", {
-      tokenADisplaySymbol: "DUSD",
-      tokenBDisplaySymbol: "DFI",
-      network: "TestNet",
+    cy.request({
+      method: "Post",
+      url: "/api/v0/wallet/dex-stab-fees",
+      qs: {
+        tokenADisplaySymbol: "DUSD",
+        tokenBDisplaySymbol: "DFI",
+        network: "TestNet",
+      },
     }).then((resp) => {
       expect(resp.status).to.eq(200);
-      expect(resp.body).to.deep.equal(5);
+      expect(resp.body).to.deep.equal("5.00");
     });
   });
 });
