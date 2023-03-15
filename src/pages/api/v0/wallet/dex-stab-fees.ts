6e2a238a3e309e9344f14b7c7fdaff4f72cbe4d6
veralygit <57183851+veralygit@users.noreply.github.com>
2023-03-15 14:50:26 +0800

    update type, variable names and update fees logic to check if pair exist but fees doesnt exist

--- src/pages/api/v0/wallet/dex-stab-fees.ts
+++ src/pages/api/v0/wallet/dex-stab-fees.ts
@@ -1,6 +1,10 @@
 import BigNumber from "bignumber.js";
 import { NextApiRequest, NextApiResponse } from "next";
-import { newOceanOptions, newWhaleAPIClient } from "@waveshq/walletkit-core";
+import {
+  EnvironmentNetwork,
+  newOceanOptions,
+  newWhaleAPIClient,
+} from "@waveshq/walletkit-core";
 import Cors from "cors";
 import { runMiddleware } from "../../../../utils/middleware";
 
@@ -8,12 +12,22 @@ type Fees = {
   fee: string | BigNumber;
 };
 
+type dexStabFeeRequestBody = {
+  tokenADisplaySymbol: string;
+  tokenBDisplaySymbol: string;
+  network: EnvironmentNetwork;
+};
+
+interface DexStabRequest extends NextApiRequest {
+  body: dexStabFeeRequestBody;
+}
+
 export const cors = Cors({
   methods: ["GET", "HEAD"],
 });
 
 export default async function handler(
-  req: NextApiRequest,
+  req: DexStabRequest,
   res: NextApiResponse<Fees>
 ) {
   await runMiddleware(req, res, cors);
@@ -23,7 +37,7 @@ export default async function handler(
   const whaleApiClient = newWhaleAPIClient(oceanOptions);
   const poolpairs = await whaleApiClient.poolpairs.list(200);
 
-  const pairWithFees = poolpairs.filter(
+  const pairWithDUSD = poolpairs.filter(
     (pair) =>
       pair.displaySymbol.includes("DUSD") &&
       pair.displaySymbol.includes(tokenBDisplaySymbol) &&
@@ -31,12 +45,16 @@ export default async function handler(
   );
 
   let fee;
-  if (pairWithFees.length === 0) {
+  if (
+    pairWithDUSD.length === 0 ||
+    (pairWithDUSD[0].tokenB.fee == undefined &&
+      pairWithDUSD[0].tokenA.fee === undefined)
+  ) {
     fee = "0";
   } else {
-    fee = pairWithFees[0].tokenB.fee
-      ? pairWithFees[0].tokenB.fee?.pct
-      : pairWithFees[0].tokenA.fee?.pct;
+    fee = pairWithDUSD[0].tokenB.fee
+      ? pairWithDUSD[0].tokenB.fee?.pct
+      : pairWithDUSD[0].tokenA.fee?.pct;
   }
 
   res.send(fee === "0" ? fee : new BigNumber(fee).multipliedBy(100).toFixed(2));
