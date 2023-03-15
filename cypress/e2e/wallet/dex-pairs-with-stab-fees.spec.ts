9a1ffc64731d1f4df01e517cc831a3c3f7c8f635
veralygit <57183851+veralygit@users.noreply.github.com>
2023-03-15 14:19:27 +0800

    update cypress request to get

--- cypress/e2e/wallet/dex-pairs-with-stab-fees.spec.ts
+++ cypress/e2e/wallet/dex-pairs-with-stab-fees.spec.ts
@@ -1,6 +1,6 @@
 describe("Wallet - Dex Stabilisation Fee", () => {
   it("should be able to return actual data", () => {
-    cy.request("POST", "/api/v0/wallet/dex-pairs-with-stab-fees").then(
+    cy.request("GET", "/api/v0/wallet/dex-pairs-with-stab-fees").then(
       (resp) => {
         expect(resp.status).to.eq(200);
         expect(resp.body).to.deep.equal([
