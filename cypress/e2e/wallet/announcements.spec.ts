0fba561b9bab8d7c85be09688269da5f551953fa
Mikhail <mykhailo@cakedefi.com>
2023-06-16 14:28:38 +0800

    chore(e2e): added response verifications for /flags and /announcements apis; (#67)
    added response verifications for /flags and /announcements apis;

--- cypress/e2e/wallet/announcements.spec.ts
+++ cypress/e2e/wallet/announcements.spec.ts
@@ -1,8 +1,13 @@
 describe("Wallet - Announcements", () => {
   it("should be able to return actual data", () => {
-    cy.request("/api/v0/announcements").then((resp) => {
-      expect(resp.status).to.eq(200);
-      expect(Array.isArray(resp.body)).eq(true);
+    cy.fixture('announcements').then((announcementsData) => {
+      cy.request("/api/v0/announcements").then((resp) => {
+        expect(resp.status).to.eq(200);
+        expect(Array.isArray(resp.body)).eq(true);
+        expect(resp.body).to.deep.equal(announcementsData);
+      });
     });
   });
 });
+
+
--- cypress/e2e/wallet/flags.spec.ts
+++ cypress/e2e/wallet/flags.spec.ts
@@ -1,8 +1,11 @@
 describe("Wallet - Flags", () => {
   it("should be able to return actual data", () => {
-    cy.request("/api/v0/settings/flags").then((resp) => {
-      expect(resp.status).to.eq(200);
-      expect(Array.isArray(resp.body)).eq(true);
+    cy.fixture('flags').then((flagsData) => {
+      cy.request("/api/v0/settings/flags").then((resp) => {
+        expect(resp.status).to.eq(200);
+        expect(Array.isArray(resp.body)).eq(true);
+        expect(resp.body).to.deep.equal(flagsData);
+      });
     });
   });
 });
