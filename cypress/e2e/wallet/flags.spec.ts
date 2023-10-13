260e639d70de9ce26145116b676092959a32fee3
Pierre Gee <pierre.gabaldon@gmail.com>
2023-10-13 14:41:16 +0800

    chore(core): add evm flag (#107)
    * chore(core): add evm flag
    
    * test yml
    
    * test yml

--- cypress/e2e/wallet/flags.spec.ts
+++ cypress/e2e/wallet/flags.spec.ts
@@ -1,6 +1,6 @@
 describe("Wallet - Flags", () => {
   it("should be able to return actual data", () => {
-    cy.fixture('flags').then((flagsData) => {
+    cy.fixture("flags").then((flagsData) => {
       cy.request("/api/v0/settings/flags").then((resp) => {
         expect(resp.status).to.eq(200);
         expect(Array.isArray(resp.body)).eq(true);
