c33e63407e9ca790f19a2c811e98a0fb4f31a542
chloe <44501120+chloezxyy@users.noreply.github.com>
2024-04-18 15:03:31 +0800

    chore(dep): remove save_tx tag

--- src/pages/api/v0/settings/flags.ts
+++ src/pages/api/v0/settings/flags.ts
@@ -374,15 +374,5 @@ export default async function handle(
       platforms: ["ios", "android", "web"],
       app: ["MOBILE_LW"],
     },
-    {
-      id: "save_tx",
-      name: "Allow to save tx",
-      stage: "public",
-      version: ">=2.37.0",
-      description: "Allow to save tx in mainnet",
-      networks: [EnvironmentNetwork.MainNet],
-      platforms: ["ios", "android", "web"],
-      app: ["MOBILE_LW"],
-    },
   ]);
 }
